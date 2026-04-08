1a1f5babc165ea3fa03be58dc4d47f86bd4adac9
NS SIDD TUTORIAL <76404154+nssidd0@users.noreply.github.com>
2026-04-08 22:30:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
-self.addEventListener('install', (e) => {
-  // Yahan maine 'v2' kar diya hai
-  e.waitUntil(
-    caches.open('ns-sidd-store-v2').then((cache) => cache.addAll([
-      './index.html',
-    ])),
-  );
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDuj1rzRh1MxqePXlXYYnT9mbM6R05O--Y",
+  authDomain: "ns-sidd-notifications.firebaseapp.com",
+  projectId: "ns-sidd-notifications",
+  storageBucket: "ns-sidd-notifications.firebasestorage.app",
+  messagingSenderId: "17196140740",
+  appId: "1:17196140740:web:0d1f81dca27d9d283ae191"
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+const messaging = firebase.messaging();
+
+// Background notification handler
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: './logo.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
